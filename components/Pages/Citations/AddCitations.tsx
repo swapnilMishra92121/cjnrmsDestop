4359b1db2e10deceb4d99177e3441bbdebe2ac05
swapnil.mishra@qsstechnosoft.com <swapnil.mishra@qsstechnosoft.com>
2024-12-03 12:51:13 +0530

    .

--- components/Pages/Citations/AddCitations.tsx
+++ components/Pages/Citations/AddCitations.tsx
@@ -20,175 +20,173 @@ import { ButtonComponents } from "@/components/CommonComponents/Fields/Button/Bu
 import { openNotificationWithIcon } from "@/components/CommonComponents/Toster/Toster";
 import { successAddedMessage } from "@/utils/const";
 
-
-
-const { SplitView, GridView, Setting, newLogo, theme ,account} = images;
+const { SplitView, GridView, Setting, newLogo, theme, account } = images;
 
 export const AddCitations: React.FC = () => {
-  const [activeBtn, setActiveBtn] = useState<number | null>(null)
+  const [activeBtn, setActiveBtn] = useState<number>(0);
   const [activeTab, setActiveTab] = useState<number>(1);
   const [glanceView, setGlanceView] = useState<boolean>(false);
   const [formData, setformData] = useState<FormData>({
     Vehicles: {
-      plate: null,
-      state: null,
-      expiration: null,
+      plate: "",
+      state: "",
+      expiration: "",
       noPlate: false,
       twentyOneDayPlate: false,
-      make: null,
-      model: null,
-      year: null,
-      color: null,
-      style: null,
-      type: null,
-      vin: null,
+      make: "",
+      model: "",
+      year: "",
+      color: "",
+      style: "",
+      type: "",
+      vin: "",
       isCommercialVehicle: true,
       hasHazardousMaterial: false,
-      dotNumber: null,
-      poundsOverWeight: null,
-      occupants: null,
+      dotNumber: "",
+      poundsOverWeight: "",
+      occupants: "",
       hasMotorcycle: false,
       hasTrailer: false,
       is16PlusPass: false,
     },
     Subject: {
-      plate: null,
-      identificationType: null,
-      subjectType: null,
-      dlState: null,
+      plate: "",
+      identificationType: "",
+      subjectType: "",
+      dlState: "",
       cdl: false,
       parked: false,
-      lastName: null,
-      firstName: null,
-      middleName: null,
-      suffix: null,
-      address: null,
-      apt: null,
-      city: null,
-      state: null,
-      zip: null,
-      race: null,
-      gender: null,
-      dob: null,
-      age: null,
+      lastName: "",
+      firstName: "",
+      middleName: "",
+      suffix: "",
+      address: "",
+      apt: "",
+      city: "",
+      state: "",
+      zip: "",
+      race: "",
+      gender: "",
+      dob: "",
+      age: "",
       isJuvenileCourtOffense: false,
-      juvenileOffenseType: null,
-      height: null,
-      weight: null,
-      hair: null,
-      eyes: null,
+      juvenileOffenseType: "",
+      height: "",
+      weight: "",
+      hair: "",
+      eyes: "",
       driver: false,
       owner: false,
       citee: false,
       passenger: false,
-      LicenseNumber: null
+      LicenseNumber: "",
     },
     Location: {
-      address: null,
-      apt: null,
-      city: null,
-      state: null,
-      zip: null,
-      weather: null,
-      direction: null,
-      parkingMeterNumber: null,
-      meterType: null,
-      zoneType: null,
-      hasStopLocation: null,
-      stopLocationAddress: null,
-      stopLocationApt: null,
-      stopLocationCity: null,
-      stopLocationState: null,
-      stopLocationZip: null,
+      address: "",
+      apt: "",
+      city: "",
+      state: "",
+      zip: "",
+      weather: "",
+      direction: "",
+      parkingMeterNumber: "",
+      meterType: "",
+      zoneType: "",
+      hasStopLocation: "",
+      stopLocationAddress: "",
+      stopLocationApt: "",
+      stopLocationCity: "",
+      stopLocationState: "",
+      stopLocationZip: "",
     },
     Violation: {
       endangerLifeOrProperty: true,
-      category: null,
-      statusType: null,
-      searchStatueOrOrdinance: null,
-      searchDescription: null,
-      nibrsCode: null,
-      level: null,
+      category: "",
+      statusType: "",
+      searchStatueOrOrdinance: "",
+      searchDescription: "",
+      nibrsCode: "",
+      level: "",
       violations: [
         {
           thirdViolation: true,
-          statueOrOrdinance: null,
-          description: null,
+          statueOrOrdinance: "",
+          description: "",
         },
       ], // Initialize as an array of objects
       addThirdViolation: true,
-      speed: null,
-      zone: null,
-      disobey: null,
-      acTaken: null,
-      acTesType: null,
-      acReading: null,
-      status: null,
+      speed: "",
+      zone: "",
+      disobey: "",
+      acTaken: "",
+      acTesType: "",
+      acReading: "",
+      status: "",
       speciesNumber: false,
-      speciesNumberValue: null,
+      speciesNumberValue: "",
       wildlifeRestitution: false,
     },
     Notes: {
-      comments: null,
-      incidentSummary: null,
-      mode: null,
-      otherMethod: null,
-      lock: null,
-      pbtNumber: null,
-      squadNumber: null,
+      comments: "",
+      incidentSummary: "",
+      mode: "",
+      otherMethod: "",
+      lock: "",
+      pbtNumber: "",
+      squadNumber: "",
       isInDashVideoAvailable: true,
-      observations:null,
-      Residential :false,
-      Rural:false,
+      observations: "",
+      Residential: false,
+      Rural: false,
       Divided: false,
       Other: false,
       ImpairedVisibility: false,
       TrafficPresent: false,
       Freeway: false,
-      Slippery:false,
-      CauseToDodge:false,
-      Rain:false,
-      Snow:false,
-      Fog:false,
-      ConditionOther:false,
-      ViolatorDirection : null,
-      Lane:null,
-      Method:null,
-      SquadDirection:null,
-      SquadNumber:null,
-      audio :false,     
+      Slippery: false,
+      CauseToDodge: false,
+      Rain: false,
+      Snow: false,
+      Fog: false,
+      ConditionOther: false,
+      ViolatorDirection: "",
+      Lane: "",
+      Method: "",
+      SquadDirection: "",
+      SquadNumber: "",
+      audio: false,
       Video: false,
-      ObservationVehicleOverPosted : false,
-      AudoClear :false,
-      AlwaysInSight : false,
-      OtherTraffic:null,
-      SingleTarget : false,
-      otherTarget :null, 
-      Terrain: null,
-      SeatBelt : false,
-      WarningOther: null,
-      Insurance : null,    
-      meeting:false,
-      Following:false,
-      AtStop:false,
-      Admitted:false,
-      otherWarning: null,
-      NoOtherTraffic:false,
+      ObservationVehicleOverPosted: false,
+      AudoClear: false,
+      AlwaysInSight: false,
+      OtherTraffic: "",
+      SingleTarget: false,
+      otherTarget: "",
+      Terrain: "",
+      SeatBelt: false,
+      WarningOther: "",
+      Insurance: "",
+      meeting: false,
+      Following: false,
+      AtStop: false,
+      Admitted: false,
+      otherWarning: "",
+      NoOtherTraffic: false,
     },
     CitationInfo: {
-      citationType: null,
-      deliveryMethod: null,
-      offenseDate: null,
-      offenseTime: null,
-      officer: null,
-      badge: null,
-      caseOrICRNumber: null,
-      county: null,
-      prosecutingCourt: null,
-      prosecutingEntity: null,
+      citationType: "",
+      deliveryMethod: "",
+      offenseDate: "",
+      offenseTime: "",
+      officer: "",
+      badge: "",
+      caseOrICRNumber: "",
+      county: "",
+      prosecutingCourt: "",
+      prosecutingEntity: "",
       mandatoryCourt: false,
-    }
-  })
+    },
+  });
 
   return (
     <>
@@ -202,20 +200,17 @@ export const AddCitations: React.FC = () => {
         <Flex gap="small" vertical wrap>
           <Flex gap="middle" justify="space-between" align="center">
             {/* <h4 style={{ display: "flex" }}>Add Citation</h4> */}
-            <Image
-              alt="Logo"
-              src={newLogo}
-              height={100}
-              width={150}
-            />
+            <Image alt="Logo" src={newLogo} height={100} width={150} />
             <Flex gap="small">
               <ButtonComponents
                 name="Impound"
                 showBackgroundColor={activeBtn === 1 ? true : false}
                 color={activeBtn === 1 ? "#00FFFF" : "gray"}
                 textColor={activeBtn === 1 ? "#fff" : "gray"}
-                borderColor={activeBtn===1 ? "gray" : "gray"}
-                handleClick={() => { setActiveBtn(1) }}
+                borderColor={activeBtn === 1 ? "gray" : "gray"}
+                handleClick={() => {
+                  setActiveBtn(1);
+                }}
               />
 
               <ButtonComponents
@@ -223,8 +218,10 @@ export const AddCitations: React.FC = () => {
                 showBackgroundColor={activeBtn === 2 ? true : false}
                 color={activeBtn === 2 ? "#00FFFF" : "gray"}
                 textColor={activeBtn === 2 ? "#fff" : "gray"}
-                borderColor={activeBtn===2 ? "gray" : "gray"}
-                handleClick={() => { setActiveBtn(2) }}
+                borderColor={activeBtn === 2 ? "gray" : "gray"}
+                handleClick={() => {
+                  setActiveBtn(2);
+                }}
               />
 
               <ButtonComponents
@@ -232,8 +229,10 @@ export const AddCitations: React.FC = () => {
                 showBackgroundColor={activeBtn === 3 ? true : false}
                 color={activeBtn === 3 ? "#00FFFF" : "gray"}
                 textColor={activeBtn === 3 ? "#fff" : "gray"}
-                borderColor={activeBtn===3 ? "gray" : "gray"}
-                handleClick={() => { setActiveBtn(3) }}
+                borderColor={activeBtn === 3 ? "gray" : "gray"}
+                handleClick={() => {
+                  setActiveBtn(3);
+                }}
               />
             </Flex>
             <Flex gap="small" align="center">
@@ -270,14 +269,9 @@ export const AddCitations: React.FC = () => {
 
               <Tooltip title="theme" placement="bottom">
                 <Button
-                  onClick={() => { }}
+                  onClick={() => {}}
                   icon={
-                    <Image
-                      src={theme}
-                      alt="grid view"
-                      height={20}
-                      width={20}
-                    />
+                    <Image src={theme} alt="grid view" height={20} width={20} />
                   }
                   // style={{ border: glanceView ? "1px solid #4096ff" : "none" }}
                   title="Grid View"
@@ -286,7 +280,7 @@ export const AddCitations: React.FC = () => {
 
               <Tooltip title="account" placement="bottom">
                 <Button
-                  onClick={() => { }}
+                  onClick={() => {}}
                   icon={
                     <Image
                       src={account}
@@ -303,7 +297,7 @@ export const AddCitations: React.FC = () => {
           </Flex>
 
           {!glanceView && (
-            <Flex gap="middle" vertical wrap >
+            <Flex gap="middle" vertical wrap>
               <TabsComponents
                 activeTab={activeTab}
                 tabList={[
@@ -317,21 +311,33 @@ export const AddCitations: React.FC = () => {
                 handleTabChange={setActiveTab}
               />
 
-              {activeTab === 0 && <Subject setformData={setformData} formData={formData} />}
-              {activeTab === 1 && <Vehicles setformData={setformData} formData={formData} />}
+              {activeTab === 0 && (
+                <Subject setformData={setformData} formData={formData} />
+              )}
+              {activeTab === 1 && (
+                <Vehicles setformData={setformData} formData={formData} />
+              )}
               {activeTab === 2 && (
-                <Location
-                  formData={formData}
+                <Location formData={formData} setformData={setformData} />
+              )}
+              {activeTab === 3 && (
+                <Violations setformData={setformData} formData={formData} />
+              )}
+              {activeTab === 4 && (
+                <CitationInformation
                   setformData={setformData}
+                  formData={formData}
                 />
               )}
-              {activeTab === 3 && <Violations setformData={setformData} formData={formData} />}
-              {activeTab === 4 && <CitationInformation setformData={setformData} formData={formData} />}
-              {activeTab === 5 && <Notes setformData={setformData} formData={formData} />}
+              {activeTab === 5 && (
+                <Notes setformData={setformData} formData={formData} />
+              )}
             </Flex>
           )}
 
-          {glanceView && <GlanceView setformData={setformData} formData={formData} />}
+          {glanceView && (
+            <GlanceView setformData={setformData} formData={formData} />
+          )}
         </Flex>
       </div>
     </>
--- components/Pages/Citations/AddCitationsI.ts
+++ components/Pages/Citations/AddCitationsI.ts
@@ -1,69 +1,69 @@
 import { CitationInfoI } from "./Tabs/CitationInformation/Components/CitationInfoStateI";
 import { LocationI } from "./Tabs/Location/components/LocationFormI";
 import { NotesI } from "./Tabs/Notes/Components/NotesStateI";
-import {  ViolationDetailsI } from "./Tabs/Violations/Components/Violationl";
+import { ViolationDetailsI } from "./Tabs/Violations/Components/Violationl";
 
 export interface VehicleData {
-  plate: string | null;
-  state: string | null;
-  expiration: string | null;
-  noPlate: boolean | null;
+  plate: string;
+  state: string;
+  expiration: string;
+  noPlate: boolean;
   twentyOneDayPlate: boolean;
-  make: string | null;
-  model: string | null;
-  year: string | null;
-  color: string | null;
-  style: string | null;
-  type: string | null;
-  vin: string | null;
+  make: string;
+  model: string;
+  year: string;
+  color: string;
+  style: string;
+  type: string;
+  vin: string;
   isCommercialVehicle: boolean;
   hasHazardousMaterial: boolean;
-  dotNumber: string | null; 
-  poundsOverWeight: string | null;
-  occupants: string | null;
+  dotNumber: string;
+  poundsOverWeight: string;
+  occupants: string;
   hasMotorcycle: boolean;
   hasTrailer: boolean;
   is16PlusPass: boolean;
 }
 
 export interface SubjectData {
-  plate: string | null;
-  identificationType: string | null; // Assuming it's an ID type as string
-  subjectType: string | null; // Assuming it's a type as string
-  dlState: string | null;
+  plate: string;
+  identificationType: string; // Assuming it's an ID type as string
+  subjectType: string; // Assuming it's a type as string
+  dlState: string;
   cdl: boolean;
   parked: boolean;
-  lastName: string | null;
-  firstName: string | null;
-  middleName: string | null;
-  suffix: string | null; // Assuming suffix is a string
-  address: string | null;
-  apt: string | null;
-  city: string | null;
-  state: string | null;
-  zip: string | null;
-  race: string | null;
-  gender: string | null;
-  dob: string | null; // Use `Date | string` if date formats vary
-  age: string | null; // Use `number | string` if age can be numerical
+  lastName: string;
+  firstName: string;
+  middleName: string;
+  suffix: string; // Assuming suffix is a string
+  address: string;
+  apt: string;
+  city: string;
+  state: string;
+  zip: string;
+  race: string;
+  gender: string;
+  dob: string; // Use `Date | string` if date formats vary
+  age: string; // Use `number | string` if age can be numerical
   isJuvenileCourtOffense: boolean;
-  juvenileOffenseType: string | null;
-  height: string | null;
-  weight: string | null;
-  hair: string | null;
-  eyes: string | null;
+  juvenileOffenseType: string;
+  height: string;
+  weight: string;
+  hair: string;
+  eyes: string;
   driver: boolean;
   owner: boolean;
   citee: boolean;
   passenger: boolean;
-  LicenseNumber: string | null;
+  LicenseNumber: string;
 }
 
 export interface FormData {
   Vehicles: VehicleData;
   Subject: SubjectData;
-  Location:LocationI;
-  Violation:ViolationDetailsI;
-  Notes:NotesI;
+  Location: LocationI;
+  Violation: ViolationDetailsI;
+  Notes: NotesI;
   CitationInfo: CitationInfoI;
 }
