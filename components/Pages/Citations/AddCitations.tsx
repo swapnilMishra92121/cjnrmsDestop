14260c5dff33f525e4c271fcf29e56704b5206b2
Anuj <singhanuj2507@gmail.com>
2024-12-09 17:28:17 +0530

    minor changes

--- components/Pages/Citations/AddCitations.tsx
+++ components/Pages/Citations/AddCitations.tsx
@@ -226,7 +226,7 @@ export const AddCitations: React.FC = () => {
         />
       )}
 
-      <ModalComponent open={true} innerContant={<Devicedetail />} />
+      <ModalComponent open={false} innerContant={<Devicedetail />} />
 
       <div
         className="citation"
